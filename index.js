8fe3c1d9e5dc010d44a6d69b102d8cf89d6d60d4
brovis3243524 <153992827+brovis3243524@users.noreply.github.com>
2026-09-01 14:02:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const mineflayer = require('mineflayer');
 
-// Create a lightweight HTTP server so Render keeps the service active
 http.createServer((req, res) => {
   res.write("Bot is running!");
   res.end();
@@ -19,11 +18,9 @@ function createBot() {
   bot.on('spawn', () => {
     console.log('Bot joined the server!');
     
-    // Auto-register / auto-login for cracked/AuthMe setups
     bot.chat('/register BotPassword123 BotPassword123');
     bot.chat('/login BotPassword123');
 
-    // Jump every 30 seconds to stay active
     setInterval(() => {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 500);
